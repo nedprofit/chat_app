bf9598c97caa1ef0f50fbfe8e1c63e9f6a58f14b
Nouara <nedprofit@gmail.com>
2022-04-13 10:02:48 +0300

    Navbar and Rooms

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,11 @@
 Rails.application.routes.draw do
+  resources :rooms
   root 'pages#home'
   devise_for :users
+  get 'user/:id', to: 'users#show', as: 'user'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
   devise_scope :users do
     get 'users', to: 'device/session#new'
   end
-  # Defines the root path route ("/")
-  # root "articles#index"
 end
+
