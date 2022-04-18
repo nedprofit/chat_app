01aca5e0cd84b8847e5763b55f97b823e6ae224b
Nouara <nedprofit@gmail.com>
2022-04-18 14:22:21 +0300

    add messages

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
-  resources :rooms
+  resources :rooms do
+    resources :messages
+  end
   root 'pages#home'
   devise_for :users
   get 'user/:id', to: 'users#show', as: 'user'
